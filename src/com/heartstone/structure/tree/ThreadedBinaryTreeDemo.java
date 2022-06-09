88ecf1073dd9a93ad4571d635760d2830ec38b91
live <d7wywbm@dingtalk.com>
2022-06-09 17:06:48 +0800

    修改提交

--- src/com/heartstone/structure/tree/ThreadedBinaryTreeDemo.java
+++ src/com/heartstone/structure/tree/ThreadedBinaryTreeDemo.java
@@ -7,7 +7,34 @@ package com.heartstone.structure.tree;
 public class ThreadedBinaryTreeDemo {
 
     public static void main(String[] args) {
+        ThreadedBinaryNode node1 = new ThreadedBinaryNode(1, "one");
+        ThreadedBinaryNode node2 = new ThreadedBinaryNode(3, "three");
+        ThreadedBinaryNode node3 = new ThreadedBinaryNode(6, "six");
+        ThreadedBinaryNode node4 = new ThreadedBinaryNode(8, "eight");
+        ThreadedBinaryNode node5 = new ThreadedBinaryNode(10, "ten");
+        ThreadedBinaryNode node6 = new ThreadedBinaryNode(14, "fourteen");
 
+        ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
+        threadedBinaryTree.setRoot(node1);
+        node1.setLeft(node2);
+        node1.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+//        System.out.println("中序线索转换后的二叉树");
+//        threadedBinaryTree.infixThreadedNode();
+//        System.out.printf("left=%s, leftType=%d \n", node6.getLeft(), node6.getLeftType());
+//        System.out.printf("right=%s, rightType=%d \n", node6.getRight(), node6.getRightType());
+//        threadedBinaryTree.infixOrder();
+//        threadedBinaryTree.perThreadedNode();
+//        System.out.println("前序线索转换后的二叉树");
+//        System.out.printf("left=%s, leftType=%d \n", node4.getLeft(), node4.getLeftType());
+//        threadedBinaryTree.preOrder();
+        threadedBinaryTree.sufThreadedNode();
+        System.out.println("后续序线索转换后的二叉树");
+//        System.out.printf("left=%s, leftType=%d \n", node3.getLeft(), node3.getLeftType());
+//        System.out.printf("right=%s, rightType=%d \n", node3.getRight(), node3.getRightType());
+        threadedBinaryTree.sufOrder();
     }
 
 }
@@ -23,7 +50,125 @@ class ThreadedBinaryTree {
         this.root = root;
     }
 
+    public void perThreadedNode() {
+        preThreadedNode(root);
+    }
 
+    public void infixThreadedNode() {
+        infixThreadedNode(root);
+    }
+
+    public void sufThreadedNode(){
+        sufThreadedNode(root);
+    }
+
+    //后序遍历线索二叉树,需要建立当前节点的父节点
+    public void sufOrder(){
+        ThreadedBinaryNode node = root;
+    }
+
+    //前序遍历线索二叉树
+    public void preOrder() {
+        ThreadedBinaryNode node = root;
+        while (node != null) {
+            System.out.println(node);
+            while (node.getLeftType() == 0) {
+                node = node.getLeft();
+                System.out.println(node);
+            }
+            while (node.getRightType() == 1) {
+                node = node.getRight();
+                System.out.println(node);
+            }
+            node = node.getRight();
+        }
+    }
+
+    //中序线索二叉树遍历
+    public void infixOrder() {
+        ThreadedBinaryNode node = root;
+        while (node != null) {
+            //先左节点
+            while (node.getLeftType() == 0) {
+                node = node.getLeft();
+            }
+            //这个是要找的当前节点
+            System.out.println(node);
+            //再右节点
+            while (node.getRightType() == 1) {
+                node = node.getRight();
+                //这是下一节点
+                System.out.println(node);
+            }
+            //如果没有则自然向后移动
+            node = node.getRight();
+        }
+    }
+
+    private void sufThreadedNode(ThreadedBinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        sufThreadedNode(node.getLeft());
+        sufThreadedNode(node.getRight());
+        if (node.getLeft() == null) {
+            node.setLeft(pre);
+            node.setLeftType(1);
+        }
+        if (pre != null && pre.getRight() == null) {
+            pre.setRight(node);
+            pre.setRightType(1);
+        }
+        pre = node;
+    }
+
+    private void preThreadedNode(ThreadedBinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        //处理当前节点
+        if (node.getLeft() == null) {
+            node.setLeft(pre);
+            node.setLeftType(1);
+        }
+        if (pre != null && pre.getRight() == null) {
+            pre.setRight(node);
+            pre.setRightType(1);
+        }
+        pre = node;
+        //处理左节点
+        if (node.getLeftType() != 1) {
+            preThreadedNode(node.getLeft());
+        }
+        //处理右节点
+        if (node.getRightType() != 1) {
+            preThreadedNode(node.getRight());
+        }
+    }
+
+    //将二叉树节点转换成中序线索二叉树节点
+    private void infixThreadedNode(ThreadedBinaryNode node) {
+
+        if (node == null) {
+            return;
+        }
+        //左子节点转换
+        infixThreadedNode(node.getLeft());
+        //当前节点转换
+        //前驱节点
+        if (node.getLeft() == null) {
+            node.setLeft(pre);
+            node.setLeftType(1);
+        }
+        //后继节点
+        if (pre != null && pre.getRight() == null) {
+            pre.setRight(node);
+            pre.setRightType(1);
+        }
+        pre = node;
+        //右子节点转换
+        infixThreadedNode(node.getRight());
+    }
 }
 
 class ThreadedBinaryNode {
@@ -32,9 +177,9 @@ class ThreadedBinaryNode {
     private String name;
     private ThreadedBinaryNode left;
     private ThreadedBinaryNode right;
-    //左指针类别 0 左子节点 1前继节点
+    //左指针类别 0 左子节点 1前驱节点
     private int leftType;
-    //右指针类别 0 右子节点 1后驱节点
+    //右指针类别 0 右子节点 1后继节点
     private int rightType;
 
     public ThreadedBinaryNode(int id, String name) {
