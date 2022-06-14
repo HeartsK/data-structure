7cf42144ed5a02d63611038af088a47130b83a97
live <d7wywbm@dingtalk.com>
2022-06-14 18:27:12 +0800

    二分查找修改提交

--- src/com/heartstone/structure/algorithm/BinarySearch.java
+++ src/com/heartstone/structure/algorithm/BinarySearch.java
@@ -9,7 +9,8 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9};
-        int i = binarySearch(2, arr, 0, arr.length-1);
+//        int i = binarySearch(2, arr, 0, arr.length-1);
+        int i = binarySearchNoRecursive(arr, -1);
         System.out.println(i);
     }
 
@@ -28,5 +29,27 @@ public class BinarySearch {
         }else {
             return index;
         }
+    }
+
+    /**
+     * 非递归二分查询
+     * @param arr 待查询数组
+     * @param findVal 带查询值
+     * @return 查询数据下标
+     */
+    public static int binarySearchNoRecursive(int[] arr, int findVal){
+        int left = 0;
+        int right = arr.length-1;
+        while (left <= right){
+            int index= (left+right)/2;
+            if (arr[index] == findVal){
+                return index;
+            }else if (arr[index] > findVal){
+                right = index-1;
+            }else if (arr[index] < findVal){
+                left = index+1;
+            }
+        }
+        return -1;
     }
 }
